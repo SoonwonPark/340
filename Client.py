0146584b4c766e7af93560469d9e17adf1a039c1
Soonwon Park <soondei88@gmail.com>
2018-04-17 21:47:09 -0500

    made Client class

--- Client.py
+++ Client.py
@@ -1,6 +1,24 @@
 import socket
 import sys
 
+
+# Present status
+# - can do basic GET request
+# - e.g. python Client.py http://stevetarzia.com/index
+
+# To do
+# - handling errors like 301, 302 and >=400
+# - chain of mutiple redirect
+# - dealing with port number, now using 80
+# - Check the response's content-type header.
+#   Print the body to stdout only if the content-type begins with "text/html".
+#   Otherwise, exit with a non-zero exit code.
+# - Return an error code if the input url does not start with "http://"
+# - You should not require a slash at the end of top-level urls.
+
+
+
+
 # making an argument for URL based on user's typing
 # e.g. python Client.py http://northwestern.edu/somepage.html in Piazza
 for url in sys.argv[1:]:
@@ -10,41 +28,50 @@ for url in sys.argv[1:]:
 print("user input: " + url)
 
 
-def extract_address(input):
-    if input.startswith("http://"):
-        third_slash = input.find('/', 7)
-        address = input[7:third_slash]
-        return address
-    if input.startswith("https://"):
-        exit("error: use 'http://'")
+class Client:
+    # constructor
+    def __init__(self, urlinput):
+        self.address = urlinput
+        self.host = ""
+        self.hostip = ""
+        self.path = ""
 
-def extract_path(input):
-    third_slash = input.find('/', 7)
-    path = input[third_slash:]
-    return path
+    # set address, host, hostip, and path
+    def set_ahhp(self):
+        if self.address.startswith("http://"):
+            third_slash = self.address.find('/', 7)
+            self.path = self.address[third_slash:]
+            self.address = self.address[7:third_slash]
+            self.host = socket.getfqdn(self.address)
+            self.hostip = socket.gethostbyname(self.host)
+
+        if self.address.startswith("https://"):
+            exit("error: use 'http://'")
 
 
-# set hostip, path
-address = extract_address(url)
-host = socket.getfqdn(address)
-hostip = socket.gethostbyname(host)
-path = extract_path(url)
+
+
+
+# construct a Client class with url
+myClient = Client(url)
+myClient.set_ahhp()
+
 
 # print url for test
-print("host: " + host)
-print("host IP: " + hostip)
-print("path: " + path)
+print("host: " + myClient.host)
+print("host IP: " + myClient.hostip)
+print("path: " + myClient.path)
 
 # make a socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the server with port 80
-mysocket.connect((hostip, 80))
+mysocket.connect((myClient.hostip, 80))
 
-print("connected!")
+print("Trying to connect!")
 
 # making a request on HTTP format
-request = "GET "+ path + " HTTP/1.1\r\nHost: " + host+"\r\n\r\n"
+request = "GET "+ myClient.path + " HTTP/1.1\r\nHost: " + myClient.host+"\r\n\r\n"
 print(request)
 
 # send request to server
