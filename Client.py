cd179f8d4284a1e5c6dc1526f9a8cb786d6ae4f9
Soonwon Park <soondei88@gmail.com>
2018-04-17 20:14:33 -0500

    Trial 1

--- Client.py
+++ Client.py
@@ -1,20 +1,19 @@
 import socket
 import sys
 
-# making parameter based on user
+# making an argument for URL based on user's typing
 # e.g. python Client.py http://northwestern.edu/somepage.html in Piazza
-for userinput in sys.argv[1:]:
-    userinput = userinput
+for url in sys.argv[1:]:
+    url = url
 
-# # address test
-print("user input: " + userinput)
+# print url for test
+print("user input: " + url)
 
 
 def extract_address(input):
     if input.startswith("http://"):
         third_slash = input.find('/', 7)
         address = input[7:third_slash]
-        print("address: " + address)
         return address
     if input.startswith("https://"):
         exit("error: use 'http://'")
@@ -22,34 +21,34 @@ def extract_address(input):
 def extract_path(input):
     third_slash = input.find('/', 7)
     path = input[third_slash:]
-    print("path: " + path)
     return path
 
 
-address = extract_address(userinput)
-path = extract_path(userinput)
+# set hostip, path
+address = extract_address(url)
+host = socket.getfqdn(address)
+hostip = socket.gethostbyname(host)
+path = extract_path(url)
 
+# print url for test
+print("host: " + host)
+print("host IP: " + hostip)
+print("path: " + path)
 
 # make a socket
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.getfqdn(address)
-hostip = socket.gethostbyname(host)
-print("host: " + host)
-print("host IP: " + hostip)
 
-# # host and port should be decided
-# # now localhost
-# # host = mysocket.set
-# port = 1247
-#
-# # connect to the server
+# connect the server with port 80
 mysocket.connect((hostip, 80))
-# # mysocket.create_connection(host)
-#
-# print mysocket.recv(4096)
-#
-# # test for sending message
-# inpt = raw_input('type anything and enter... ')
-# mysocket.send(inpt)
-#
-# print "your message has been sent"
+
+print("connected!")
+
+# making a request on HTTP format
+request = "GET "+ path + " HTTP/1.1\r\nHost: " + host+"\r\n\r\n"
+print(request)
+
+# send request to server
+mysocket.send(request)
+
+# print response from the server
+print mysocket.recv(4096)
