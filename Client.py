e6d3f7c90f5f8c10c922a56caec5851591c4ddae
Soonwon Park <soondei88@gmail.com>
2018-04-21 13:43:49 -0500

    test

--- Client.py
+++ Client.py
@@ -50,32 +50,101 @@ class Client:
 
 
 
+class ManageSocket:
+
+    def __init__(self, input_hostip, input_path, input_host):
+        self.hostip = input_hostip
+        self.minesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.request = "GET "+ input_path + " HTTP/1.1\r\nHost: " + input_host+"\r\n\r\n"
+        self.recvmessage = ""
+        self.status_code = ""
+        self.redirect = ""
+
+    def connect(self):
+        print("Trying to Connect")
+        self.minesocket.connect((self.hostip, 80))
+
+    def sendrequest(self):
+        print(self.request)
+        self.minesocket.send(self.request)
+
+    def receive(self):
+        self.recvmessage = self.minesocket.recv(4096)
+        # print("Print Original Message")
+        # print(self.recvmessage)
+        # print("Print Extracted Message")
+        self.status_code = self.recvmessage[9:12]
+        # print("status code: " + self.status_code)
+
+    def foward(self):
+        if self.status_code == "301" or self.status_code == "302" :
+            Location_start = self.recvmessage.find("Location")
+            Location_end = self.recvmessage.find("\r", Location_start)
+            print("redirected to: " + self.recvmessage[Location_start:Location_end])
+            
+
+        if self.status_code == "200" :
+            print(self.recvmessage)
+            # print("Success")
 
 
-# construct a Client class with url
-myClient = Client(url)
-myClient.set_ahhp()
 
 
-# print url for test
-print("host: " + myClient.host)
-print("host IP: " + myClient.hostip)
-print("path: " + myClient.path)
 
-# make a socket
-mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect the server with port 80
-mysocket.connect((myClient.hostip, 80))
+def main():
 
-print("Trying to connect!")
+    myClient2 = Client(url)
+    myClient2.set_ahhp()
 
-# making a request on HTTP format
-request = "GET "+ myClient.path + " HTTP/1.1\r\nHost: " + myClient.host+"\r\n\r\n"
-print(request)
+    mysocket2 = ManageSocket(myClient2.hostip, myClient2.path, myClient2.host)
+    mysocket2.connect()
+    mysocket2.sendrequest()
+    mysocket2.receive()
+    mysocket2.foward()
 
-# send request to server
-mysocket.send(request)
 
-# print response from the server
-print mysocket.recv(4096)
+
+
+
+
+
+# run main function
+main()
+
+
+
+
+
+
+
+
+
+#
+# # construct a Client class with url
+# myClient = Client(url)
+# myClient.set_ahhp()
+#
+#
+# # print url for test
+# print("host: " + myClient.host)
+# print("host IP: " + myClient.hostip)
+# print("path: " + myClient.path)
+#
+# # make a socket
+# mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# # connect the server with port 80
+# mysocket.connect((myClient.hostip, 80))
+#
+# print("Trying to connect!")
+#
+# # making a request on HTTP format
+# request = "GET "+ myClient.path + " HTTP/1.1\r\nHost: " + myClient.host+"\r\n\r\n"
+# print(request)
+#
+# # send request to server
+# mysocket.send(request)
+#
+# # print response from the server
+# print mysocket.recv(4096)
